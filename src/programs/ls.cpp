f00a7050aa25632112c2a42473756c6d3b7bd2d2
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2019-04-26 17:41:16 +0200

    Hakuna matata 6

--- src/programs/ls.cpp
+++ src/programs/ls.cpp
@@ -8,38 +8,44 @@
 #include <cstring>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <sstream>
+#include <array>
+#include <fstream>
 
 using namespace std;
 
-int Ls(const string &args, Process &proc, Channel *io)
-{
-	ostream &out = io->out();
-
-	pid_t pid = 0;
-	int pipefd[2];
-	FILE* output;
-	char line[256];
-	int status;
-	if(pipe(pipefd) != 0) {
-		throw std::runtime_error(strerror(errno));
+std::string exec(const char * cmd) {
+	std::array < char, 128 > buffer;
+	std::string result;
+	std::unique_ptr < FILE, decltype( & pclose) > pipe(popen(cmd, "r"), pclose);
+	if (!pipe) {
+		throw std::runtime_error("popen() failed!");
 	}
-	pid = fork();
-	if (pid == 0) {
-		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT_FILENO);
-		dup2(pipefd[1], STDERR_FILENO);
-
-		execl("/bin/ls", "/bin/ls", "-l",  static_cast<const string &>(proc.env_.get_wd()).c_str(), (char*) NULL);
-		exit(0);
-	} else if (pid > 0) {
-		close(pipefd[1]);
-		output = fdopen(pipefd[0], "r");
-
-		while(fgets(line, sizeof(line), output)) {
-			out << line;
-		}
-
-		waitpid(pid, &status, 0);
+	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+		result += buffer.data();
 	}
+	return result;
+}
+
+int Ls(const string & args, Process & proc, Channel * io) {
+	ostream & out = io->out();
+
+	string path = static_cast<const string &>(proc.env_.get_wd()).c_str();
+	string cmdstr = "/bin/ls -l " + path;
+
+	const char * cmd = cmdstr.c_str();
+	auto pipe = popen(cmd, "r");
+
+	char c;
+	while ((c = fgetc(pipe)) != EOF) {
+		out << c;
+	}
+
+	pclose(pipe);
+
 	return 0;
 }
